7872ad0eebca7cc0b91793e59087e32701dacec9
Sofia Medina <medinasofiaa1@gmail.com>
2023-03-31 12:36:02 -0300

    segunda parte practica 1.6

--- algoritmo.js
+++ algoritmo.js
@@ -1,7 +1,22 @@
-let nombre = prompt ("cual es tu nombre?") 
+let numeroEntero = prompt("Ingresar el tipo de dato integer/entero:");
+let booleano = prompt("Ingresar el tipo de dato booleano:");
+let numeroDecimal = prompt("Ingresar un numero decimal:");
+let caracter = prompt("Ingresar un caracter:");
+let palabra = prompt("Ingresar una palabra:");
+
+document.write("<h1> Entero: "+ numeroEntero + "</h1>");
+document.write("<h2> Booleano: "+ booleano + "</h2>");
+document.write("<h3> NumeroDecimal: "+ numeroDecimal + "</h3>");
+document.write("<h4> Cartacter: "+ caracter + "</h4>");
+document.write("<h5> Palabra(conjunto de caracteres): "+ palabra + "</h5>");
+
+
+
+
+/* let nombre = prompt ("cual es tu nombre?") 
 
 document.querySelector("body").innerHTML = "<h1> " + nombre + " </h1>"; 
 
-document.write("<h2> Mi nombre es "+ nombre +" </h2>");
+document.write("<h2> Mi nombre es "+ nombre +" </h2>"); */
 
 
